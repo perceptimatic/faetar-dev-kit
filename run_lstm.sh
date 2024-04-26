e86fa5e1dac2008b3566ef087c18ff0c08a17efc
michaelong7 <michael.ong@mail.utoronto.ca>
2024-04-26 19:33:13 +0000

    added evaluate code from mms prep

--- run_lstm.sh
+++ run_lstm.sh
@@ -2,8 +2,6 @@
 
 exp_dir="exp_lstm"
 
-. utils/parse_options.sh
-
 if [ $# -ne 2 ]; then
   echo "Usage: $0 data-dir out-dir"
   exit 1
@@ -16,46 +14,59 @@ if [ ! -d "$1" ]; then
   exit 1
 fi
 
-if [ ! -d "$2" ]; then
-  echo "$0: '$2' is not a directory"
-  exit 1
-fi
-
 data_dir="$1"
 out_dir="$2"
+conf_dir="conf_lstm"
 
 mkdir -p "$exp_dir"/"$out_dir"
 
-if [ ! -d ""$exp_dir"/"$out_dir"/conf" ]; then
-    cp -r conf "$exp_dir"/"$out_dir"/
+if [ ! -d ""$exp_dir"/"$out_dir"/"$conf_dir"" ]; then
+    cp -r "$conf_dir" "$exp_dir"/"$out_dir"/
 fi
 
+# fixes vocab size
+awk -v number="$(wc -l < "$data_dir"/prep/token2id)" \
+'NR == 5 {print "vocab_size: " number} NR != 5 {print $0}' "$exp_dir"/"$out_dir"/"$conf_dir"/model.yaml \
+> "$exp_dir"/"$out_dir"/"$conf_dir"/model.yaml_
+mv "$exp_dir"/"$out_dir"/"$conf_dir"/model.yaml{_,}
 
-awk -v number="$(awk '{number = 0; if ($2 > number) {number = $2}} END {print number + 1}' token2id)" \
-'NR == 5 {print "vocab_size: " number} NR != 5 {print $0}' "$exp_dir"/"$out_dir"/conf/model.yaml > "$exp_dir"/"$out_dir"/conf/model.yaml_
-mv "$exp_dir"/"$out_dir"/conf/model.yaml{_,}
 if [ ! -f ""$exp_dir"/"$out_dir"/best.ckpt" ]; then
     python3 prep/asr-baseline.py \
-    --read-model-yaml "$exp_dir"/"$out_dir"/conf/model.yaml \
+    --read-model-yaml "$exp_dir"/"$out_dir"/"$conf_dir"/model.yaml \
     train \
     --state-dir "$exp_dir"/"$out_dir"/ --state-csv "$exp_dir"/"$out_dir"/history.csv \
-    --read-data-yaml "$exp_dir"/"$out_dir"/conf/data.yaml \
-    --read-training-yaml "$exp_dir"/"$out_dir"/conf/training.yaml \
+    --read-data-yaml "$exp_dir"/"$out_dir"/"$conf_dir"/data.yaml \
+    --read-training-yaml "$exp_dir"/"$out_dir"/"$conf_dir"/training.yaml \
     "$data_dir"/train "$data_dir"/dev "$exp_dir"/"$out_dir"/best.ckpt
 fi
-for x in test train; do
-    if [ ! -f ""$exp_dir"/"$out_dir"/error_report_"$x"" ]; then
+for x in test dev train; do
+    if [ ! -f ""$exp_dir"/"$out_dir"/trn_dec_"$x"" ]; then
         # no lm
         python3 prep/asr-baseline.py \
-        --read-model-yaml "$exp_dir"/"$out_dir"/conf/model.yaml \
+        --read-model-yaml "$exp_dir"/"$out_dir"/"$conf_dir"/model.yaml \
         decode \
-        --read-data-yaml "$exp_dir"/"$out_dir"/conf/data.yaml \
+        --read-data-yaml "$exp_dir"/"$out_dir"/"$conf_dir"/data.yaml \
         "$exp_dir"/"$out_dir"/best.ckpt "$data_dir"/"$x" "$exp_dir"/"$out_dir"/hyp_"$x"
 
         torch-token-data-dir-to-trn \
-        "$exp_dir"/"$out_dir"/hyp_"$x" --swap token2id "$exp_dir"/"$out_dir"/trn_dec_"$x"
-
-        python3 prep/error-rates-from-trn.py \
-        trn_"$x" "$exp_dir"/"$out_dir"/trn_dec_"$x" > "$exp_dir"/"$out_dir"/error_report_"$x"
+        "$exp_dir"/"$out_dir"/hyp_"$x" --swap "$data_dir"/prep/token2id "$exp_dir"/"$out_dir"/trn_dec_"$x"
     fi
+
+    for y in per cer wer; do
+      if [ ! -f ""$exp_dir"/"$out_dir"/error_report_eval_"$x"_"$y"" ]; then
+        tempfile1="$(mktemp)"
+        tempfile2="$(mktemp)"
+
+        awk 'BEGIN {print "file_name,sentence"} {gsub(/[\(\)]/, "", $NF); filename=$NF; NF--; print filename","$0}' "$data_dir"/prep/trn_"$x" > "$tempfile1"
+        awk 'BEGIN {print "file_name,sentence"} {gsub(/[\(\)]/, "", $NF); filename=$NF; NF--; print filename","$0}' "$exp_dir"/"$out_dir"/trn_dec_"$x" > "$tempfile2"
+
+        python3 ./step.py evaluate --error-type "$y" \
+        "$tempfile1" "$tempfile2" > "$exp_dir"/"$out_dir"/error_report_eval_"$x"_"$y"
+      fi
+    done
+    
+    # if [ ! -f ""$exp_dir"/"$out_dir"/error_report_"$x"" ]; then
+    #     python3 prep/error-rates-from-trn.py \
+    #     "$data_dir"/prep/trn_"$x" "$exp_dir"/"$out_dir"/trn_dec_"$x" > "$exp_dir"/"$out_dir"/error_report_"$x"
+    # fi
 done
