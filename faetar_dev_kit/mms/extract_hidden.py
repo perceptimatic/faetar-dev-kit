59b627e6defbdc934283553f71ad438429ef9fce
Leo Peckham <padril@gra-login3.graham.sharcnet>
2024-05-21 18:49:11 -0400

    Fixed extract_hidden.py

--- faetar_dev_kit/mms/extract_hidden.py
+++ faetar_dev_kit/mms/extract_hidden.py
@@ -20,35 +20,49 @@ from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 from tqdm import tqdm
 
 def extract_hidden(hidden_dir, model_dir, data_dir, lang):
+    hidden_dir = Path(hidden_dir).resolve()
+    model_dir = Path(model_dir).resolve()
+    data_dir = Path(data_dir).resolve()
+
     model = Wav2Vec2ForCTC.from_pretrained(model_dir, target_lang=lang,
+                                           output_hidden_states=True,
                                            ignore_mismatched_sizes=True)
 
     processor = Wav2Vec2Processor.from_pretrained(model_dir, target_lang=lang,
                                                   ignore_mismatched_sizes=True)
 
+    sampling_rate=processor.feature_extractor.sampling_rate
+
+    print("Loading dataset")
     data = load_dataset("audiofolder", data_dir=data_dir, split="all")
+    print("Casting dataset to audio")
     data = data.cast_column(
-        "audio", Audio(sampling_rate=processor.feature_extractor.sampling_rate)
+        "audio", Audio(sampling_rate=sampling_rate)
     )
 
     os.makedirs(hidden_dir, exist_ok=True)
+    states = None
 
     for audio in tqdm(data["audio"]):
-        path = os.path.splitext(Path(audio["path"]).relative_to(data_dir).as_posix())[0]
+        path = hidden_dir / os.path.splitext(Path(audio["path"]).relative_to(data_dir).as_posix())[0]
+
+        if os.path.exists(f"{path}_48.pt"):
+            continue
 
         inputs = processor(
-            audio,
-            sampling_rate=processor.feature_extractor.sampling_rate,
+            audio["array"],
+            sampling_rate=sampling_rate,
             return_tensors="pt",
             padding=True,
         )
+        states = model(inputs.input_values).hidden_states
 
-        for i, hidden in enumerate(model(inputs.input_values).hidden_states):
-            pt = hidden_dir / (path + f"_{i}.pt")
+        for i in [12, 24, 36, 46, 47, 48]:
+            pt = f"{path}_{i}.pt"
+            os.makedirs(os.path.split(path)[0], exist_ok=True)
+            torch.save(states[i], pt)
 
-            torch.save(hidden, pt)
-
-    return 0
+    return states
 
 if __name__ == "__main__":
     _, hidden, model, data, lang, *_ = sys.argv
