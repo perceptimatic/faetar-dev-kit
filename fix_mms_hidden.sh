47d6b2ecea492b92d6f38df6c45bd419252c77e2
michaelong7 <michael.ong@mail.utoronto.ca>
2024-06-13 17:18:46 +0000

    organized output of extract_hidden

--- fix_mms_hidden.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-for part in test train dev; do
-    for layer in 12 24 36 46 47 48; do
-        mkdir -p data/mms_hidden/layer_$layer/$part/feat/
-        python3 squeeze_mms.py "data/mms_hidden/$part/" "$layer" "$part"
-    done
-done
