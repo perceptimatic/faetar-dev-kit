93f3b8729c7eae2a6a2e5b550b4b0cb759a3eb8a
michaelong7 <michael.ong@mail.utoronto.ca>
2024-06-27 21:05:23 +0000

    can now set out_dir

--- faetar_dev_kit/boothroyd/section_data.sh
+++ faetar_dev_kit/boothroyd/section_data.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 perp_file hypothesis_trn_file num-to-split"
+if [ $# -ne 4 ]; then
+    echo "Usage: $0 perp_file hypothesis_trn_file num-to-split out-dir"
     exit 1
 fi
 
 perp_file="$1"
 hyp_trn="$2"
 ns="$3"
+out_dir="$4"
 
 if [ ! -f "$perp_file" ]; then
     echo "'$perp_file' is not a file"
@@ -16,6 +17,11 @@ fi
 
 if [ ! -f "$hyp_trn" ]; then
     echo "'$hyp_trn' is not a file"
+    exit 1
+fi
+
+if [ ! -d "$out_dir" ]; then
+    echo "'$out_dir' is not a directory"
     exit 1
 fi
 
@@ -42,7 +48,7 @@ function split_text() {
 
 awk '{print $0"\t"NR}' "$perp_file" |
 sort -n -k 1,1 |
-awk -v ns="$ns" -v lines="$(wc -l < "$perp_file")" -v filename="$(basename "$perp_file")" -v dirname="$(dirname "$perp_file")" \
+awk -v ns="$ns" -v lines="$(wc -l < "$perp_file")" -v filename="$(basename "$perp_file")" -v out_dir="$out_dir" \
 'BEGIN {
     FS = "\t";
     OFS = " ";
@@ -52,7 +58,7 @@ NR == FNR {
     cut = 0.05;
 
     for (i = 1; i <= ns; i++) {
-        out_ref_path = dirname "/" i "_ref_" filename
+        out_ref_path = out_dir "/" i "_ref_" filename
         if ((NR >= (lines * cut)) && (NR <= (lines * (1 - cut)))) {
             if ((NR > ((lines * (1 - 2 * cut)) * ((i - 1) / ns) + (lines * cut))) && (NR <= ((lines * (1 - 2 * cut)) * (i / ns) + (lines * cut)))) {
                 print $4 "\t" $2, $3 > out_ref_path;
@@ -65,7 +71,7 @@ NR == FNR {
 
 NR != FNR {
     for (i = 1; i <= ns; i++) {
-        out_hyp_path = dirname "/" i "_hyp_" filename
+        out_hyp_path = out_dir "/" i "_hyp_" filename
         for (ind_name in extract) {
             if (ind_name == FNR "_" i) {
                 print $0 > out_hyp_path;
@@ -79,11 +85,11 @@ NR != FNR {
     }
 }' "-" "$hyp_trn"
 
-for file in "$(dirname "$perp_file")"/*_hyp_*; do
+for file in "$out_dir"/*_hyp_*; do
     split_text "$file" 
 done
 
-for file in "$(dirname "$perp_file")"/*_ref_*; do
+for file in "$out_dir"/*_ref_*; do
     sort -nk 1,1 "$file" |
     awk 'BEGIN {FS = "\t"} {print $2}' > "$file"_
     mv "$file"{_,}
